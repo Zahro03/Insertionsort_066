1e4cb1d81525d2d601ef6c0aca7d7aa2aa74ebdf
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-03-24 15:41:01 +0700

    menginput data ke-

--- Belajar insertion sort/Belajar insertion sort.cpp
+++ Belajar insertion sort/Belajar insertion sort.cpp
@@ -20,4 +20,14 @@ void input() {
 		cout << "Masukan Element Array" << endl;
 		cout << "=======================" << endl;
 	}
-	
+	for (int i = 0; i = n; i++)
+	{
+		cout << "data ke-" << (i + 1) << ":  ";
+		cin >> arr[i];
+	}
+
+}
+
+
+
+
