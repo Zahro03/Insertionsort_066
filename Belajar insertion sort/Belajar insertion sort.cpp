255b3c251b88c28fd0c3e580bb283391fd739037
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-03-24 15:44:57 +0700

    menjalankan temporari

--- Belajar insertion sort/Belajar insertion sort.cpp
+++ Belajar insertion sort/Belajar insertion sort.cpp
@@ -35,6 +35,17 @@ void insertionsort() {
 		temp = arr[i];
 
 		j = i = 1;
+		while (j >= 0 && arr[j] > temp)
+
+		{
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = temp;
+
+		cout << "\nPass " << i << ": ";
+		
+
 
 		
 
